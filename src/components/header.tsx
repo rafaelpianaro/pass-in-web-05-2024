bced4a2a3b9c3806faf9403e7b598c5984994a2c
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-05-06 20:47:59 -0300

    feat: make nav link component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
 	return (
 		<div className='flex items-center gap-5 py-2'>
 			<img src={nlwUniteIcon} alt="Logotipo com imagem do nlw unite" />
 			<nav className='flex items-center gap-5'>
-				<a className='font-medium text-sm text-zinc-400' href="#">Eventos</a>
-				<a className='font-medium text-sm' href="#">Participantes</a>
+				{/* <a className='font-medium text-sm text-zinc-400' href="#">Eventos</a> */}
+				<NavLink title='Link para acessar eventos.' href='/eventos'>Eventos</NavLink>
+				<NavLink href='/participantes'>Participantes</NavLink>
+				<NavLink>Outro link</NavLink>
 			</nav>
 		</div>
 	)
