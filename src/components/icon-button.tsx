b990f234db82da44e203dfbd89f4bb497a4d3cf6
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-05-07 23:07:26 -0300

    feat: create functions for pagination and change styles on icon-button component

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,15 +1,26 @@
 import { ComponentProps } from "react";
 
 interface IconButtonProps extends ComponentProps<'button'> {
-    transparent?: boolean
+    transparent?: boolean;
 }
 
-export function IconButton({ transparent, ...props}: IconButtonProps) {
+export function IconButton({ transparent, ...props }: IconButtonProps) {
+    // Extrai `disabled` das props para usar na lógica de estilo
+    const { disabled } = props;
+
+    const baseStyle = 'border rounded-md p-1.5';
+    const enabledStyle = transparent
+        ? 'bg-black/20 border-white/10'
+        : 'bg-white/10 border-white/10';
+    const disabledStyle = 'bg-gray-400 text-gray-800 cursor-not-allowed';
+
+    // Define a classe do botão com base no estado disabled
+    const buttonClass = `${baseStyle} ${disabled ? disabledStyle : enabledStyle}`;
+
     return (
-        <button {...props} 
-            className={transparent
-                ? 'bg-black/20 border border-white/10 rounded-md p-1.5'
-                : 'bg-white/10 border border-white/10 rounded-md p-1.5'
-            } />
-    )
+        <button
+            {...props}
+            className={buttonClass}
+        />
+    );
 }
